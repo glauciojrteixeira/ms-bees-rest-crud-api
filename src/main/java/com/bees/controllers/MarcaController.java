f7d7fa07d4747a19917c81f59651edee7d09520f
glauciojrteixeira <glauciojrteixeira@gmail.com>
2023-02-09 14:15:58 -0300

    Controller Marca: buscarTodas e buscarTodasPaginado.

--- src/main/java/com/bees/controllers/MarcaController.java
+++ src/main/java/com/bees/controllers/MarcaController.java
@@ -6,9 +6,15 @@ import com.bees.services.MarcaService;
 import com.bees.services.VersionAPI;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/marcas")
@@ -32,4 +38,45 @@ public class MarcaController {
         // Antes do response, converte o objeto de dominio em um DTO
         return ResponseEntity.ok().body(new MarcaDTO(body));
     }
+
+    @ApiOperation(value="Busca todos os registros")
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<List<MarcaDTO>> buscarTodas(
+            @RequestHeader(name = "api-version", defaultValue = "0", required = false) String versionHeader,
+            @RequestParam(value = "version", defaultValue = "0", required = false) String versionParam) {
+
+        List<Marca> body = marcaService.buscarTodas(
+                VersionAPI.version(versionHeader, versionParam));
+
+        // Converte a lista do objeto de dominio em uma lista DTO
+        List<MarcaDTO> bodyDTO = body
+                .stream()
+                .map(categoria -> new MarcaDTO(categoria))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(bodyDTO);
+    }
+
+    @ApiOperation(value="Busca todos os registros com paginação")
+    @GetMapping(value = "/page",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Page<MarcaDTO>> buscarPaginado(
+            @RequestHeader(name = "api-version", defaultValue = "0", required = false) String versionHeader,
+            @RequestParam(value = "version", defaultValue = "0", required = false) String versionParam,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nomeMarca") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+        Page<Marca> body = marcaService.buscarTodasPaginado(
+                VersionAPI.version(versionHeader, versionParam),
+                page, linesPerPage, orderBy, direction);
+
+        // Converte a lista do objeto de dominio em uma lista DTO
+        Page<MarcaDTO> bodyDTO = body
+                .map(marca -> new MarcaDTO(marca));
+
+        return ResponseEntity.ok().body(bodyDTO);
+    }
+
 }
--- src/main/java/com/bees/services/MarcaService.java
+++ src/main/java/com/bees/services/MarcaService.java
@@ -6,8 +6,12 @@ import com.bees.services.exceptions.ObjetoNaoEncontradoException;
 import com.bees.services.exceptions.VersionAPIException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,6 +33,30 @@ public class MarcaService {
             return obj.orElseThrow(
                     () -> new ObjetoNaoEncontradoException("Objeto não encontrado! "
                             + "[ " + Marca.class.getSimpleName() + " | Código: " + id + " ]"));
+        } else {
+            throw new VersionAPIException(MSG_API_NAO_ENCONTRADA);
+        }
+    }
+
+    public List<Marca> buscarTodas(String version) {
+
+        version = version.equals("0") ? versionAPIDefault : version;
+
+        if (version.equals("1.0")) {
+            return marcaRepo.findAll();
+        } else {
+            throw new VersionAPIException(MSG_API_NAO_ENCONTRADA);
+        }
+    }
+
+    public Page<Marca> buscarTodasPaginado(String version,
+                                           Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        version = version.equals("0") ? versionAPIDefault : version;
+
+        if (version.equals("1.0")) {
+            return marcaRepo.findAll(pageRequest);
         } else {
             throw new VersionAPIException(MSG_API_NAO_ENCONTRADA);
         }
